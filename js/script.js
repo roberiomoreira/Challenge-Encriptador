467864f995dc01826e8430d96d8e45c7e52f1fc2
roberio <roberiom.barboza@gmail.com>
2023-01-23 17:48:41 -0300

    Bugs corrigidos e código rodando sem erros.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
+
+
 let textInicial = document.querySelector(".text-no-encrypt");
 let textFinal = document.querySelector(".text-encrypt");
 
 function btnEncriptar(){
-    const text = encriptar(textInicial.value);    
+    const text = encriptar(textInicial.value);        
+    
+    textFinal.value = text;
+    textInicial.value = "";
+
+}
+
+function btnDesencriptar(){
+    const text = desencriptar(textInicial.value);
 
     textFinal.value = text;
     textInicial.value = "";
 
+}
+
+function btnCopiarEColar(){
+    textInicial.value = textFinal.value;
+    textFinal.value = "";
 }
 
 function encriptar(texto){
@@ -14,12 +29,23 @@ function encriptar(texto){
     
     texto = texto.toLowerCase();
     
-    console.log(matrizCodigo);
-    
     for(let i = 0; i < matrizCodigo.length; i++){
         if(texto.includes(matrizCodigo[i][0])){
             texto = texto.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
         }
+    }
+    return texto;
+}
+
+function desencriptar(texto){
+    let letters = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]; 
+    
+    for(let i = 0; i < letters.length; i++){
+        
+        if(texto.includes(letters[i][1])){
+            
+            texto = texto.replaceAll(letters[i][1], letters[i][0]);                
+        }        
     }
     return texto;
 }
